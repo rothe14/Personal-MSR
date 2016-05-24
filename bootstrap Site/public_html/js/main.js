c91b2b922aff724a13a2f1ebf8ae13d07cbeb0a0
Matthew S Rothe <matthewsrothe@gmail.com>
2016-05-24 12:31:07 -0500

    Initial Website Load
    This is the initial website, further changes will be made to the top of
    this.

---
+++ bootstrap Site/public_html/js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    setTimeout(function(){
+       $('body').addClass('loaded');
+
+    }, 3000);
+
+    //function to smooth scroll to selected Div section
+    $("a.navLink").on('click', function(event) {
+        event.preventDefault();
+
+        var hash = this.hash;
+
+        $("html, body").animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function() {
+            window.location.hash = hash;
+        });
+    });
+    //function to shrink navbar when scrolling down
+    $(window).scroll(function() {
+        if ($(document).scrollTop() > 50) {
+            $('nav').addClass('small');
+            $('#back-to-top').fadeIn();
+        } else {
+            $('nav').removeClass('small');
+            $('#back-to-top').fadeOut();
+        }
+    });
+    //function to scroll back to top when pressing button
+    $('#back-to-top').click(function() {
+        $('#back-to-top').tooltip('hide');
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+
+    $('#back-to-top').tooltip('show');
+});
